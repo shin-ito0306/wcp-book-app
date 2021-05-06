9846070a38ebf5f693142341db760c764599928d
shin-ito0306 <shin.20000306@icloud.com>
2021-05-06 11:04:53 +0000

    [add] validates messages

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -27,6 +27,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy

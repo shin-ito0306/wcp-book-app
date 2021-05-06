f1f2a50f6f35d3ec133cbf0141522c97457315e3
shin-ito0306 <shin.20000306@icloud.com>
2021-05-06 08:27:51 +0000

    [fix] edit

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -13,13 +13,19 @@ class BooksController < ApplicationController
     book.save
     redirect_to book_path(book.id)
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
-    
+
   end
 
   private
